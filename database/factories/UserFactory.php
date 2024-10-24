eb10efc1a250dfeec1c9b391104cd4c90eea91b3
Arseni <Arsenij522@mail.ru>
2024-10-24 21:44:11 +0300

    update user and his factory
    update user and his factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,16 +21,34 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    
+    private function getRandomImagePath() : string {
+        $imageDirectory = public_path('images/factory');
+        $image_paths = array_diff(scandir($imageDirectory), ['.', '..']);
+
+        try {
+            $randomImagepath = 'images/factory/' . $image_paths[array_rand($image_paths)];            
+        } catch (\Exception $e) {
+            $randomImagepath = 'optimized_optimized_1729794763.jpg';
+        }
+
+
+        return $randomImagepath;
+    }
+    
     public function definition(): array
-    {
+    {        
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'phone_number' => fake()->phoneNumber(),
+            'picture_path' => $this -> getRandomImagePath(),
         ];
     }
+    
 
     /**
      * Indicate that the model's email address should be unverified.
